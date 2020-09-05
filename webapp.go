c97ee5882248c8984326d0caed501a0b11d888ff
Anthony Hein <anhein@princeton.edu>
2020-09-04 20:44:44 -0400

    First commit <3

---
+++ webapp.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "io"
+    "net/http"
+    "fmt"
+)
+
+func main() {
+    http.HandleFunc("/info", info)
+    http.Handle("/", http.FileServer(http.Dir("./notes")))
+    http.ListenAndServe(":443", nil)
+}
+
+func info(res http.ResponseWriter, req *http.Request) {
+    io.WriteString(res, `This is a Golang server that simply servers beautiful HTML files capturing my class notes that were exported from Typora, a markdown editor.`)
+}
