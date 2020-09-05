b1dec16e0d4c0396d5718a8df7e547bb97bac944
Anthony Hein <anhein@princeton.edu>
2020-09-04 21:08:50 -0400

    Added Godeps.

--- webapp.go
+++ webapp.go
@@ -2,16 +2,33 @@
 package main
 
 import (
-    "io"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"io"
 )
 
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func main() {
-    http.HandleFunc("/info", info)
-    http.Handle("/", http.FileServer(http.Dir("./notes")))
-    http.ListenAndServe(":443", nil)
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/info", info)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
 
 func info(res http.ResponseWriter, req *http.Request) {
-    io.WriteString(res, `This is a Golang server that simply servers beautiful HTML files capturing my class notes that were exported from Typora, a markdown editor.`)
+	io.WriteString(res, `This is a Golang server that simply servers beautiful HTML files capturing my class notes that were exported from Typora, a markdown editor.`)
 }
